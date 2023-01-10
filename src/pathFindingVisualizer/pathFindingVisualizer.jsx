c8019eb888bbcea5c04ce7a99ad12009a8aabb22
gkost1 <garrettkost@outlook.com>
2023-01-09 20:35:55 -0600

    working on getting start/end node

--- src/pathFindingVisualizer/pathFindingVisualizer.jsx
+++ src/pathFindingVisualizer/pathFindingVisualizer.jsx
@@ -5,14 +5,52 @@ import './pathFindingVisualizer.css';
 export default class pathFindingVisualizer extends Component {
     constructor(props){
         super(props)
-        this.state = {};
+        this.state = {
+            nodes:[],
+        };
+    }
+
+    componentDidMount(){
+        const nodes =[];
+        for (let row = 0; row<20; row++){
+            const currentRow = [];
+            for (let col = 0; col <50; col++){
+                const currentNode = {
+                    col,
+                    row,
+                    isStart: row === 10 && row === 5,
+                    isFinish: row === 10 && col === 45,
+                };
+                currentRow.push(currentNode);
+            }
+            nodes.push(currentRow);
+        }
+        this.setState({nodes})
     }
 
     render() {
-        return(
-            <div>
-              Foo Bar Biz
-              <Node></Node>
+        const {nodes} = this.state;
+        console.log(nodes)
+
+        return (
+            <div className = "grid">
+              {nodes.map((row,rowIdx) => {
+                return (
+                    <div key={rowIdx}>
+                        {row.map((node,nodeIdx) => {
+                            const {isStart, isFinish} = node;
+                            return (
+                                <Node>
+                                    key = {nodeIdx}
+                                    isStart={isStart}
+                                    isFinish={isFinish}
+                                    test={'foo'}
+                                    test={'kappa'}</Node>
+                            );
+                        })}
+                    </div>
+                );
+              })}
             </div>
         );   
     }
